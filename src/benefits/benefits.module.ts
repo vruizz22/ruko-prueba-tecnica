4cb1075cb4ac2adc9a88752722b1c5a715963734
VICTOR RUIZ <veruiz@uc.cl>
2025-06-30 02:38:38 -0400

    feat(benefits): agregar providers para BenefitsService y PrismaService en BenefitsModule

--- src/benefits/benefits.module.ts
+++ src/benefits/benefits.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BenefitsController } from '@benefits/benefits.controller';
+import { BenefitsService } from '@benefits/benefits.service';
+import { PrismaService } from '@/prisma.service';
 
 @Module({
   controllers: [BenefitsController],
+  providers: [BenefitsService, PrismaService],
 })
 export class BenefitsModule {}
